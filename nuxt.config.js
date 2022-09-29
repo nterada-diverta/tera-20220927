afc4ef562eb5253b7642ecf11772e590f0d0954a
nterada-diverta <nterada@diverta.co.jp>
2022-09-29 15:30:09 +0900

    updated base url

--- nuxt.config.js
+++ nuxt.config.js
@@ -33,7 +33,12 @@ export default {
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
+    '@nuxtjs/axios'
   ],
+
+  axios: {
+    baseURL: process.env.BASE_URL
+  },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
